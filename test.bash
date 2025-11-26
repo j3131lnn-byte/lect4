5b0b3f68d6b280fb42364984efb2fd4876be7f07
j3131lnn-bite <j3131lnn@sit.ac.jp>
2025-11-26 14:33:10 +0900

    ,add lisence

---
+++ test.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SPDX-FIleCopyrightText: 2025 Daiki ojima j3131lnn@sit.ac.jp
+#SPDX-License-Identifier:BSD-3-Clause
+
+ng(){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
