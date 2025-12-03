573fe6aa2d5420356048e8b453bb8472fa94e725
j3131lnn-bite <j3131lnn@sit.ac.jp>
2025-12-03 14:29:20 +0900

    add github actions

--- test.bash
+++ test.bash
@@ -10,7 +10,7 @@ ng(){
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15] || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]     || ng ${LINENO}
@@ -18,7 +18,7 @@ out=$(echo あ | ./plus)
 
 out=$(echo | ./plus)
 [ "$?" = 1 ]     || ng ${LINENO}
-[ "${out}" = ""] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
