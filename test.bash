1be9449b378d2344593f3491f69f6223d3a87d9e
j3131lnn-bite <j3131lnn@sit.ac.jp>
2025-12-10 14:05:00 +0900

    c

--- test.bash
+++ test.bash
@@ -9,16 +9,16 @@ ng(){
 
 res=0
 
-#out=$(seq 5 | ./plus)
-#[ "$out" = "15" ] || ng  ${LINENO}
+out=$(seq 5 | ./plus)
+[ "$out" = "15" ] || ng  ${LINENO}
 
-#out=$(echo あ | ./plus)
-#[ "$?" = 1 ] || ng ${LINENO}
-#[ -z "$out" ] || ng ${LINENO}
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
-#out=$(echo | ./plus)
-#[ "$?" = 1 ] || ng ${LINENO}
-#[ -z "$out" ] || ng ${LINENO}
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
